8266377cc62ee6f481eae009cca1f41332fb9c2d
nikmace <nikmacentosh@gmail.com>
2023-03-07 13:35:08 +0200

    Added favicon to head

--- theme.config.tsx
+++ theme.config.tsx
@@ -19,6 +19,7 @@ const config: DocsThemeConfig = {
   },
   head: (
     <>
+      <link rel="icon" type="image/x-icon" href="/favicon.svg" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="Morphex Docs" />
       <meta
